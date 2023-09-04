dacce9f926d99b52c14f1763187f39ef6b45e2d8
Romário Lima <devromariolima@gmail.com>
2023-09-04 16:52:46 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  // Parâmetros opcionais
+  speed: 500,
+
+  // Se precisarmos de paginação
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
